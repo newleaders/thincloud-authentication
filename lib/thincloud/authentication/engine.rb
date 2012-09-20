399d6e7fb51780c3c4b086739e022d9170c532dc
Phil Cohen <github@phlippers.net>
2012-09-20 12:03:57 -0700

    refactor omniauth initializers

--- lib/thincloud/authentication/engine.rb
+++ lib/thincloud/authentication/engine.rb
@@ -4,13 +4,36 @@ module Thincloud
     class Engine < ::Rails::Engine
       isolate_namespace Thincloud::Authentication
 
-      initializer "thincloud.authentication.omniauth" do |app|
+      initializer "thincloud.authentication.omniauth.middleware" do |app|
         require "omniauth"
         require "omniauth-identity"
 
         app.middleware.use ::OmniAuth::Builder do
           provider :identity, fields: [:email], model: Identity,
             on_failed_registration: RegistrationsController.action(:new)
+        end
+      end
+
+      initializer "thincloud.authentication.omniauth.logger" do
+        ::OmniAuth.config.logger = ::Rails.logger
+      end
+
+      initializer "thincloud.authentication.omniauth.failure_endpoint" do
+        ::OmniAuth.config.on_failure = -> env do
+          ::OmniAuth::FailureEndpoint.new(env).redirect_to_failure
+        end
+      end
+
+      initializer "thincloud.authentication.omniauth.identity_redirects" do
+        # override default omniauth-identity forms
+        class ::OmniAuth::Strategies::Identity
+          def registration_form
+            redirect "/signup"
+          end
+
+          def request_phase
+            redirect "/login"
+          end
         end
       end
 
