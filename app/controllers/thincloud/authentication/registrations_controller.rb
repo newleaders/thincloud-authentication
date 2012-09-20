07914702a03927d0d5dd166d44e1aa3af758823b
Phil Cohen <github@phlippers.net>
2012-09-20 11:18:24 -0700

    refactor registrations controller

--- app/controllers/thincloud/authentication/registrations_controller.rb
+++ app/controllers/thincloud/authentication/registrations_controller.rb
@@ -3,18 +3,20 @@ require_dependency "thincloud/authentication/application_controller"
 module Thincloud::Authentication
   # Public: Handle OmniAuth callbacks.
   class RegistrationsController < ApplicationController
+    before_filter :extract_identity, only: :create
+
     def new
       @identity = Identity.new
     end
 
     def create
       # identity exists
-      if omniauth && identity = Identity.find_omniauth(omniauth)
-        login_as identity.user
+      if @identity.present?
+        login_as @identity.user
         redirect_to main_app.root_url, notice: "You have been logged in."
       # new identity for current_user
       elsif current_user
-        current_user.identities.build.apply_omniauth(omniauth).save
+        add_omniauth_identity_to_current_user
         redirect_to main_app.root_url, notice: "You have been logged in."
       # failed identity login
       elsif invalid_identity_credentials?
@@ -22,24 +24,15 @@ module Thincloud::Authentication
                                      strategy: "identity"
       # create a new identity
       else
-        # params[:identity] exists when creating a local identity provider
-        @identity = Identity.new(params[:identity])
-        @identity.user = User.create
+        @identity = create_identity_from_request
+        render :new and return if @identity.errors.any?
 
-        # omniauth exists if coming from a 3rd party provider like LinkedIn
         if omniauth
-          @identity.apply_omniauth(omniauth)
+          login_as @identity.user
         else
-          flash[:alert] = "Welcome! Please check your email to verify your " <<
-            "registration."
+          flash[:alert] = "Check your email to verify your registration."
         end
-
-        if @identity.save
-          login_as @identity.user if omniauth
-          redirect_to main_app.root_url
-        else
-          render :new
-        end
+        redirect_to main_app.root_url
       end
     end
 
@@ -59,11 +52,38 @@ module Thincloud::Authentication
       request.env["omniauth.auth"]
     end
 
+    # Private: Set an instance varible if an `Identity` if present.
+    #
+    # Returns: An instance of `Identity` or `nil`.
+    def extract_identity
+      @identity = Identity.find_omniauth(omniauth) if omniauth
+    end
+
     # Private: Determine if the request is from an invalid Identity login.
     #
     # Returns: Boolean.
     def invalid_identity_credentials?
       params[:provider] == "identity" && params.has_key?(:auth_key)
+    end
+
+    # Private: Add a new identity to the `current_user` from OmniAuth.
+    #
+    # Returns: Boolean.
+    def add_omniauth_identity_to_current_user
+      current_user.identities.build.apply_omniauth(omniauth).save
+    end
+
+    # Private: Create a new identity from submitted params or OmniAuth.
+    #
+    # Returns: An instance of `Identity`.
+    def create_identity_from_request
+      # params[:identity] exists when creating a local identity provider
+      Identity.new(params[:identity]).tap do |identity|
+        identity.user = User.create
+        # omniauth exists if coming from a 3rd party provider like LinkedIn
+        identity.apply_omniauth(omniauth) if omniauth
+        identity.save
+      end
     end
   end
 end
