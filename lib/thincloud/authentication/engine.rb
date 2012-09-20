38afba264afb98249861312b2bfdbf4af3ab4a7d
Phil Cohen <github@phlippers.net>
2012-09-20 10:45:35 -0700

    add omniauth middleware to initializer

--- lib/thincloud/authentication/engine.rb
+++ lib/thincloud/authentication/engine.rb
@@ -7,6 +7,11 @@ module Thincloud
       initializer "thincloud.authentication.omniauth" do |app|
         require "omniauth"
         require "omniauth-identity"
+
+        app.middleware.use ::OmniAuth::Builder do
+          provider :identity, fields: [:email], model: Identity,
+            on_failed_registration: RegistrationsController.action(:new)
+        end
       end
 
       config.generators do |g|
