71a49cf20f8637ba98e0d70e0ed025f76374d7cf
Cyrus Farajpour <smoils@gmail.com>
2012-10-11 17:46:37 -0700

    break out module declarations:
    * I had an issue with including this gem on a 
      fresh rails application: uninitialized constant 
      Thincloud
    
    * fix: be more explicit with module declarations

--- lib/thincloud/authentication/configuration.rb
+++ lib/thincloud/authentication/configuration.rb
@@ -1,20 +1,22 @@
-module Thincloud::Authentication
-  class << self
-    attr_accessor :configuration
-  end
+module Thincloud
+  module Authentication
+    class << self
+      attr_accessor :configuration
+    end
 
-  def self.configure
-    self.configuration ||= Configuration.new
-    yield configuration
-  end
+    def self.configure
+      self.configuration ||= Configuration.new
+      yield configuration
+    end
 
-  # Public: Configuration options for the Thincloud::Authentication module
-  class Configuration
-    attr_accessor :providers, :mailer_sender
+    # Public: Configuration options for the Thincloud::Authentication module
+    class Configuration
+      attr_accessor :providers, :mailer_sender
 
-    def initialize
-      @providers = {}
-      @mailer_sender = "app@example.com"
+      def initialize
+        @providers = {}
+        @mailer_sender = "app@example.com"
+      end
     end
   end
 end
