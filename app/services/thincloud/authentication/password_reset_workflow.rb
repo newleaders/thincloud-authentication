823316968ddfb6db1a6596ad26bc1604e1f50d10
Phil Cohen <github@phlippers.net>
2013-05-06 13:54:29 -0700

    add password reset workflow service object

---
+++ app/services/thincloud/authentication/password_reset_workflow.rb
@@ -0,0 +1,10 @@
+module Thincloud::Authentication
+  # Public: Execute the workflow steps to reset a password for an Identity
+  class PasswordResetWorkflow
+    def self.call(email)
+      return unless identity = Identity.find_by_email(email)
+      identity.generate_password_token!
+      PasswordsMailer.password_reset(identity.id).deliver
+    end
+  end
+end
