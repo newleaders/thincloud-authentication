1c9f4d1e2bda4e2ff48605f829793dc1101ff648
Robert Bousquet <rbousquet@newleaders.com>
2013-08-14 18:07:59 -0700

    Update dummy app for Rails 4

--- test/dummy/config/initializers/secret_token.rb
+++ test/dummy/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '10f26bd4422ca14f13654103c24674b87a3a02ca0a5018de8aea342981579c4395f65989b3a31740f6dc089dd1bbefbc6e9372d96e4be8adb709b8bc66da6878'
+Dummy::Application.config.secret_key_base = '10f26bd4422ca14f13654103c24674b87a3a02ca0a5018de8aea342981579c4395f65989b3a31740f6dc089dd1bbefbc6e9372d96e4be8adb709b8bc66da6878'
